22a8249cf8a35a78c4c8caf8342e6c27505e4180
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-04-26 02:29:59 -0500

    F: Estilos css aplicados a todos los elementos desde template

--- views/modules/navegacion.php
+++ views/modules/navegacion.php
@@ -1,4 +1,4 @@
-<nav>
+<nav class="navbar">
 	<ul>
 		<!--li>
 			<a href="index.php">Inicio</a>
--- views/template.php
+++ views/template.php
@@ -6,7 +6,35 @@
 		Template
 	</title>
 
-	<style type="text/css"></style>
+	<style type="text/css">
+		body {
+			font-family: lato;
+		}
+		h1 {
+			text-align: center;
+		}
+		.navbar {
+			background: #343D46FF;
+			color: white;
+			padding: 20px 0px;
+		}
+		.navbar ul {
+			padding-left: 18vw;
+		}
+		.navbar li {
+			display: inline-block;
+			padding-left: 10vw;
+		}
+		.navbar a {
+			color: white;
+			text-decoration: none;
+		}
+
+		section {
+			width: 1000px;
+			margin: auto;
+		}
+	</style>
 
 </head>
 <body>
