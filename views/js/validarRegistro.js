4153e098c6e33d49e4d6973fad9041ad6037bf37
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-04-28 02:13:58 -0500

    B: validado email

--- views/js/validarRegistro.js
+++ views/js/validarRegistro.js
@@ -7,6 +7,7 @@ function validarRegistro()
 	var usuario = document.querySelector("#usuarioRegistro").value;
 	var password = document.querySelector("#passwordRegistro").value;
 	var email = document.querySelector("#emailRegistro").value;
+	var terminos = document.querySelector("#terms").checked;
 
 	/*Validacion de usuario*/
 	if (usuario !="") 
@@ -49,6 +50,31 @@ function validarRegistro()
 			return false;
 		}
 	}
+
+	/*Validacion de email*/
+	if (email !="") 
+	{
+		var caracteres = email.length;
+		//no incluir ningun digito especial
+		var expressionsEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+		//Validacion para evitar enviar formulario si es modificado por el html inspector
+		if (caracteres < 6) 
+		{
+			document.querySelector("label[for='emailRegistro']").innerHTML += "<br>Escriba por favor mas de 6 caracteres.";
+			return false;
+		}
+		//validacion anti-caracteres especiales
+		//y anti SQL Injection del lado cliente
+		if (!expressionsEmail.test(usuario))
+		{
+			document.querySelector("label[for='emailRegistro']").innerHTML += "<br>Por favor inserta un email válido :)";
+			return false;
+		}
+	}
+
+	/*Validacion de checkbox terminos*/
+	
+
 	return true; 
 }
 
