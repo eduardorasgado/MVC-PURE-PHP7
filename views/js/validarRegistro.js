99d7d6134f3679a1c214bbae7f40b46820e6f34c
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-04-28 00:35:18 -0500

    F: Validacion con js para usuarioRegistro

--- views/js/validarRegistro.js
+++ views/js/validarRegistro.js
@@ -9,7 +9,21 @@ function validarRegistro()
 	var email = document.querySelector("#emailRegistro").value;
 	console.log(usuario, password, email);
 
-	 
+	/*Validacion de usuario*/
+	if (usuario !="") 
+	{
+		var caracteres = usuario.length;
+		//Validacion para evitar enviar formulario si es modificado por el html inspector
+		if (caracteres >15) 
+		{
+			document.querySelector("label[for='usuarioRegistro']").innerHTML += "<br>Escriba por favor menos de 15 caracteres.";
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	} 
 }
 
 /*=====  End of VALIDAR REGISTRO  ======*/
